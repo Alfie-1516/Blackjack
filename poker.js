8ab6346a961565291e9c72ed3045d163ae1e0496
Okorocha Alfred <okorochaalfred@gmail.com>
2025-04-29 12:58:08 -0500

    Completed Day12 Hw

--- poker.js
+++ poker.js
@@ -1,5 +1,5 @@
 let bankRoll = 0;
-
+let player_turn = false; 
 const betButton = document.getElementById("bet_button");
 const betAmountInput = document.getElementById("bet_amount");
 const currentBankRoll = document.getElementById("bet_roll");
@@ -15,6 +15,17 @@ betButton.addEventListener("click", () => {
     currentBankRoll.textContent = "Please enter a valid bet amount.";
   }
 });
+function timeToBet() {
+    player_turn = !player_turn;
+    const betModal = document.getElementById("bet_modal");
+    if (player_turn === true) {
+        betModal.style.display = "block"; 
+    }
+    else {
+        betModal.style.display = "none"; 
+    }
+}
+
 
 // Function to set the bank roll
 function setBankRoll(value) {
@@ -24,4 +35,24 @@ function setBankRoll(value) {
 // Function to get the bank roll
 function getBankRoll() {
   return bankRoll;
+}
+
+function make_wager(value){
+    const betAmount = document.getElementById('raise_amount').value;
+    switch (value) {
+        case 'Raise':
+            console.log(`Raise bet: ${betAmount}`);
+            // Add logic for raising the bet
+            break;
+        case 'Call':
+            console.log(`Call bet: ${betAmount}`);
+            // Add logic for calling the bet
+            break;
+        case 'Check':
+            console.log('Check action triggered');
+            // Add logic for checking
+            break;
+        default:
+            console.error('Unknown action:', value);
+    }
 }
